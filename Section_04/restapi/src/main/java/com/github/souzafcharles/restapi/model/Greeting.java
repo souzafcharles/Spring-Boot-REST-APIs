9c9a7bf86528d628151b73f6ad4f4ccaf1e3dac1
souzafcharles <souzach@outlook.com>
2025-03-24 21:39:39 -0300

    chore: project structuring

---
+++ Section_04/restapi/src/main/java/com/github/souzafcharles/restapi/model/Greeting.java
@@ -0,0 +1,4 @@
+package com.github.souzafcharles.restapi.model;
+
+public record Greeting(Long id, String content) {
+}
---
+++ Section_04/restapi/src/test/java/com/github/souzafcharles/restapi/StartupTests.java
@@ -0,0 +1,13 @@
+package com.github.souzafcharles.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
