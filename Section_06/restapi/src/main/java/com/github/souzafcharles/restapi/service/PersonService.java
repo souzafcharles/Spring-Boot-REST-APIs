f219c7fd60bac008001208c090c5d2fc5ad92b13
Charles Fernandes de Souza <souzach@outlook.com>
2025-03-28 12:53:35 -0300

    chore: project structuring

--- Section_06/restapi/src/main/java/com/github/souzafcharles/restapi/service/PersonService.java
+++ Section_06/restapi/src/main/java/com/github/souzafcharles/restapi/service/PersonService.java
@@ -8,10 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-
 import static com.github.souzafcharles.restapi.mapper.ObjectMapper.parseListObjects;
 import static com.github.souzafcharles.restapi.mapper.ObjectMapper.parseObject;
 
